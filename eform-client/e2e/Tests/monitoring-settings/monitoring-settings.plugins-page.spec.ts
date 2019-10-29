5825289c9abb6cd85c4fb9f64a9a65020ceac4cf
Mathias Husum Nielsen <mathiashusumn@gmail.com>
2019-10-29 13:20:28 +0100

    Adding new changes to support the new plugin permission loading c# part.

--- eform-client/e2e/Tests/monitoring-settings/monitoring-settings.plugins-page.spec.ts
+++ eform-client/e2e/Tests/monitoring-settings/monitoring-settings.plugins-page.spec.ts
@@ -26,23 +26,17 @@ describe('Application settings page - site header section', function () {
 
     it('should activate the plugin', function () {
         pluginPage.pluginSettingsBtn.click();
-        browser.pause(10000);
+        browser.waitForVisible('#PluginDropDown', 40000);
         pluginPage.selectValue('PluginDropDown', 'PluginDropDown', 'Aktiveret');
-        // browser.pause(8000);
         pluginPage.saveBtn.click();
-        browser.pause(40000);
+        browser.pause(50000); // We need to wait 50 seconds for the plugin to create db etc.
         browser.refresh();
 
-        // Start - This block is here because of the new plugin permission loading, requires a re-login.
-        browser.waitForVisible('#plugin-id', 40000);
-        browser.pause(10000);
-        myEformsPage.Navbar.logout();
         loginPage.login();
         myEformsPage.Navbar.advancedDropdown();
         myEformsPage.Navbar.clickonSubMenuItem('Plugins');
         browser.waitForExist('#plugin-name', 50000);
         browser.pause(10000);
-        // End - This block is here because of the new plugin permission loading, requires a re-login.
 
         const plugin = pluginsPage.getFirstPluginRowObj();
         expect(plugin.id).equal(1);
