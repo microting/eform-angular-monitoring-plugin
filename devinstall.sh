a650b4de812e51f7d9e53eb14ad3c057bd3701ae
René Schultz Madsen <rm@microting.com>
2020-03-06 16:11:52 +0100

    Updating the devinstall.sh and devgetchanges.sh

--- devinstall.sh
+++ devinstall.sh
@@ -2,14 +2,22 @@
 cd ~
 pwd
 
-if [ -d "Documents/workspace/microting/eform-angular-frontend/eform-client/src/app/plugins/modules/monitoring-pn" ]; then
-	rm -fR Documents/workspace/microting/eform-angular-frontend/eform-client/src/app/plugins/modules/monitoring-pn
-fi
+rm -fR Documents/workspace/microting/eform-angular-frontend/eform-client/src/app/plugins/modules/monitoring-pn
 
 cp -av Documents/workspace/microting/eform-angular-monitoring-plugin/eform-client/src/app/plugins/modules/monitoring-pn Documents/workspace/microting/eform-angular-frontend/eform-client/src/app/plugins/modules/monitoring-pn
 
-if [ -d "Documents/workspace/microting/eform-angular-frontend/eFormAPI/Plugins/Monitoring.Pn" ]; then
-	rm -fR Documents/workspace/microting/eform-angular-frontend/eFormAPI/Plugins/Monitoring.Pn
-fi
+rm -fR Documents/workspace/microting/eform-angular-frontend/eFormAPI/Plugins/Monitoring.Pn
 
 cp -av Documents/workspace/microting/eform-angular-monitoring-plugin/eFormAPI/Plugins/Monitoring.Pn Documents/workspace/microting/eform-angular-frontend/eFormAPI/Plugins/Monitoring.Pn
+
+# Test files rm
+rm -fR Documents/workspace/microting/eform-angular-frontend/eform-client/e2e/Tests/monitoring-settings
+rm -fR Documents/workspace/microting/eform-angular-frontend/eform-client/e2e/Tests/monitoring-general
+rm -fR Documents/workspace/microting/eform-angular-frontend/eform-client/e2e/Page\ objects/Monitoring
+rm -fR Documents/workspace/microting/eform-angular-frontend/eform-client/wdio-plugin-step2.conf.js
+
+# Test files cp
+cp -av Documents/workspace/microting/eform-angular-monitoring-plugin/eform-client/e2e/Tests/monitoring-settings Documents/workspace/microting/eform-angular-frontend/eform-client/e2e/Tests/monitoring-settings
+cp -av Documents/workspace/microting/eform-angular-monitoring-plugin/eform-client/e2e/Tests/monitoring-general Documents/workspace/microting/eform-angular-frontend/eform-client/e2e/Tests/monitoring-general
+cp -av Documents/workspace/microting/eform-angular-monitoring-plugin/eform-client/e2e/Page\ objects/Monitoring Documents/workspace/microting/eform-angular-frontend/eform-client/e2e/Page\ objects/Monitoring
+cp -av Documents/workspace/microting/eform-angular-monitoring-plugin/eform-client/wdio-headless-plugin-step2.conf.js Documents/workspace/microting/eform-angular-frontend/eform-client/wdio-plugin-step2.conf.js
