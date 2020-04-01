f467c22fa0471d8c181e78b56db1bef8f0609000
René Schultz Madsen <rm@microting.dk>
2020-04-01 11:54:37 +0200

    Updating the sh scripts to work with chromedriver and osx.

--- testinginstallpn.sh
+++ testinginstallpn.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-sed '/\/\/ INSERT ROUTES HERE/i {' src/app/plugins/plugins.routing.ts -i
-sed '/\/\/ INSERT ROUTES HERE/i path: "monitoring-pn",' src/app/plugins/plugins.routing.ts -i
-sed '/\/\/ INSERT ROUTES HERE/i canActivate: [AuthGuard],' src/app/plugins/plugins.routing.ts -i
-sed '/\/\/ INSERT ROUTES HERE/i loadChildren: "./modules/monitoring-pn/monitoring-pn.module#MonitoringPnModule"' src/app/plugins/plugins.routing.ts -i
-sed '/\/\/ INSERT ROUTES HERE/i },' src/app/plugins/plugins.routing.ts -i
+perl -pi -e '$_.="  },\n" if /INSERT ROUTES HERE/' src/app/plugins/plugins.routing.ts
+perl -pi -e '$_.="  loadChildren: '\''./modules/monitoring-pn/monitoring-pn.module#MonitoringPnModule'\''\n" if /INSERT ROUTES HERE/' src/app/plugins/plugins.routing.ts
+perl -pi -e '$_.="  canActivate: [AuthGuard],\n" if /INSERT ROUTES HERE/' src/app/plugins/plugins.routing.ts
+perl -pi -e '$_.="  path: '\''monitoring-pn'\'',\n" if /INSERT ROUTES HERE/' src/app/plugins/plugins.routing.ts
+perl -pi -e '$_.="  {\n" if /INSERT ROUTES HERE/' src/app/plugins/plugins.routing.ts
