9a8abaa83c55a5adff502e61b4509a9a60f8a8a4
René Schultz Madsen <rm@microting.dk>
2020-04-14 20:22:07 +0200

    Fixing the module loading.

--- testinginstallpn.sh
+++ testinginstallpn.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 perl -pi -e '$_.="  },\n" if /INSERT ROUTES HERE/' src/app/plugins/plugins.routing.ts
-perl -pi -e '$_.="  loadChildren: '\''./modules/monitoring-pn/monitoring-pn.module#MonitoringPnModule'\''\n" if /INSERT ROUTES HERE/' src/app/plugins/plugins.routing.ts
-perl -pi -e '$_.="  canActivate: [AuthGuard],\n" if /INSERT ROUTES HERE/' src/app/plugins/plugins.routing.ts
-perl -pi -e '$_.="  path: '\''monitoring-pn'\'',\n" if /INSERT ROUTES HERE/' src/app/plugins/plugins.routing.ts
+perl -pi -e '$_.="      .then(m => m.MonitoringPnModule)\n" if /INSERT ROUTES HERE/' src/app/plugins/plugins.routing.ts
+perl -pi -e '$_.="    loadChildren: () => import('\''./modules/monitoring-pn/monitoring-pn.module'\'')\n" if /INSERT ROUTES HERE/' src/app/plugins/plugins.routing.ts
+perl -pi -e '$_.="    path: '\''monitoring-pn'\'',\n" if /INSERT ROUTES HERE/' src/app/plugins/plugins.routing.ts
 perl -pi -e '$_.="  {\n" if /INSERT ROUTES HERE/' src/app/plugins/plugins.routing.ts
