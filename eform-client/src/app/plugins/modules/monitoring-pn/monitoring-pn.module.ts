d7d6d5a7a64a445ab29dc762ca40705286e22bcc
Alex <sahok6264@gmail.com>
2021-04-12 22:15:25 +0300

    Refactoring
    Add states, change table headers

--- eform-client/src/app/plugins/modules/monitoring-pn/monitoring-pn.module.ts
+++ eform-client/src/app/plugins/modules/monitoring-pn/monitoring-pn.module.ts
@@ -1,32 +1,33 @@
 import { NgModule } from '@angular/core';
-import {CommonModule, registerLocaleData} from '@angular/common';
+import { CommonModule, registerLocaleData } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { TranslateModule } from '@ngx-translate/core';
 import { MDBBootstrapModule } from 'angular-bootstrap-md';
 import { NgSelectModule } from '@ng-select/ng-select';
-import { SharedPnModule } from '../shared/shared-pn.module';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { EformSharedModule } from '../../../common/modules/eform-shared/eform-shared.module';
-import {MonitoringPnLayoutComponent} from './layouts';
-import {MonitoringPnRoutingModule} from './monitoring-pn.routing.module';
-import {CalendarModule, DateAdapter} from 'angular-calendar';
-import {adapterFactory} from 'angular-calendar/date-adapters/date-fns';
+import { EformSharedModule } from 'src/app/common/modules/eform-shared/eform-shared.module';
+import { MonitoringPnLayoutComponent } from './layouts';
+import { MonitoringPnRoutingModule } from './monitoring-pn.routing.module';
+import { CalendarModule, DateAdapter } from 'angular-calendar';
+import { adapterFactory } from 'angular-calendar/date-adapters/date-fns';
 import localeDa from '@angular/common/locales/da';
-import {CasesModule} from '../../../modules';
-import {MonitoringPnNotificationRulesService, MonitoringPnSettingsService} from './services';
+import { CasesModule } from 'src/app/modules';
+import {
+  MonitoringPnNotificationRulesService,
+  MonitoringPnSettingsService,
+} from './services';
 import {
   NotificationRulesDeleteComponent,
   NotificationRulesEditComponent,
-  NotificationRulesPageComponent
+  NotificationRulesPageComponent,
 } from './components/notification-rules';
-import {MonitoringSettingsComponent} from './components/monitoring-settings';
+import { MonitoringSettingsComponent } from './components/monitoring-settings';
 
 registerLocaleData(localeDa);
 
 @NgModule({
   imports: [
     CommonModule,
-    SharedPnModule,
     MDBBootstrapModule,
     MonitoringPnRoutingModule,
     TranslateModule,
@@ -36,18 +37,20 @@ registerLocaleData(localeDa);
     FontAwesomeModule,
     CalendarModule.forRoot({
       provide: DateAdapter,
-      useFactory: adapterFactory
+      useFactory: adapterFactory,
     }),
-    CasesModule
+    CasesModule,
   ],
   declarations: [
     MonitoringSettingsComponent,
     MonitoringPnLayoutComponent,
     NotificationRulesPageComponent,
     NotificationRulesEditComponent,
-    NotificationRulesDeleteComponent
+    NotificationRulesDeleteComponent,
   ],
-  providers: [MonitoringPnSettingsService, MonitoringPnNotificationRulesService]
+  providers: [
+    MonitoringPnSettingsService,
+    MonitoringPnNotificationRulesService,
+  ],
 })
-
-export class MonitoringPnModule { }
+export class MonitoringPnModule {}
