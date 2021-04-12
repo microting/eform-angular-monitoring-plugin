6dd276df888a58c9a09cf4093100dffa72f8c1f4
Alex <sahok6264@gmail.com>
2021-04-12 18:11:54 +0300

    Fix tests

--- eform-client/e2e/Page objects/Monitoring/Monitoring-notification-rules.page.ts
+++ eform-client/e2e/Page objects/Monitoring/Monitoring-notification-rules.page.ts
@@ -1,198 +1,214 @@
 import Page from '../Page';
-import XMLForEform from '../../Constants/XMLForEform';
 import XMLForeFormExtended from '../../Constants/XMLForeFormExtended';
-import XMLForEformFractions from '../../Constants/XMLForEformFractions';
+import myEformsPage from '../MyEforms.page';
 
 export class MonitoringNotificationRulesPage extends Page {
-    constructor() {
-        super();
-    }
-    public get newEformBtn() {
-        $('#newEFormBtn').waitForDisplayed({timeout: 20000});
-        $('#newEFormBtn').waitForClickable({timeout: 20000});
-        return $('#newEFormBtn');
-    }
-    public get createEformTagSelector() {
-        $('#createEFormMultiSelector').waitForDisplayed({timeout: 20000});
-        $('#createEFormMultiSelector').waitForClickable({timeout: 20000});
-        return $('#createEFormMultiSelector');
-    }
+  constructor() {
+    super();
+  }
 
-    public get createEformNewTagInput() {
-        $('#addTagInput').waitForDisplayed({timeout: 20000});
-        $('#addTagInput').waitForClickable({timeout: 20000});
-        return $('#addTagInput');
-    }
+  public get MonitoringDropdown() {
+    const ele = $('#monitoring-pn');
+    ele.waitForDisplayed({ timeout: 20000 });
+    ele.waitForClickable({ timeout: 20000 });
+    return ele;
+  }
 
-    public get xmlTextArea() {
-        $('#eFormXml').waitForDisplayed({timeout: 20000});
-        $('#eFormXml').waitForClickable({timeout: 20000});
-        return $('#eFormXml');
-    }
+  public get monitoringPnCalendar() {
+    const ele = $('#monitoring-pn-calendar');
+    ele.waitForDisplayed({ timeout: 20000 });
+    ele.waitForClickable({ timeout: 20000 });
+    return ele;
+  }
 
-    public get createEformBtn() {
-        $('#createEformBtn').waitForDisplayed({timeout: 20000});
-        $('#createEformBtn').waitForClickable({timeout: 20000});
-        return $('#createEformBtn');
-    }
-    public get MonitoringDropdown() {
-        return $(`//*[@class= 'dropdown']//*[@id= '']`);
-    }
-    public get MonitoringRulesBtn() {
-        return $(`//*[@id= 'monitoring-pn-calendar']`);
-    }
-    public get RuleCreateBtn() {
-        $('#ruleCreateBtn').waitForDisplayed({timeout: 20000});
-        $('#ruleCreateBtn').waitForClickable({timeout: 20000});
-        return $('#ruleCreateBtn');
-    }
-    public get templateSelector() {
-        return $(`//*[@id= 'templateSelector']`);
-    }
-    public get DataFieldSelector() {
-        $('#dataFieldSelector').waitForDisplayed({timeout: 20000});
-        $('#dataFieldSelector').waitForClickable({timeout: 20000});
-        return $('#dataFieldSelector');
-    }
-    public get CheckboxOption() {
-        return $(`//*[@id= 'selected']`);
-    }
-    public get GreaterThanValue() {
-        $('#greaterThanValue').waitForDisplayed({timeout: 20000});
-        $('#greaterThanValue').waitForClickable({timeout: 20000});
-        return $('#greaterThanValue');
-    }
-    public get LessThanValue() {
-        $('#lessThanValue').waitForDisplayed({timeout: 20000});
-        $('#lessThanValue').waitForClickable({timeout: 20000});
-        return $('#lessThanValue');
-    }
-    public get EqualValue() {
-        $('#equalValue').waitForDisplayed({timeout: 20000});
-        $('#equalValue').waitForClickable({timeout: 20000});
-        return $('#equalValue');
-    }
-    public get EmailSubject() {
-        $('#emailSubject').waitForDisplayed({timeout: 20000});
-        $('#emailSubject').waitForClickable({timeout: 20000});
-        return $('#emailSubject');
-    }
-    public get EmailTextArea() {
-        $('#emailText').waitForDisplayed({timeout: 20000});
-        $('#emailText').waitForClickable({timeout: 20000});
-        return $('#emailText');
-    }
-    public get AttachReportBox() {
-        $('#attachReport').waitForDisplayed({timeout: 20000});
-        $('#attachReport').waitForClickable({timeout: 20000});
-        return $('#attachReport');
-    }
-    public get RecipientEmail() {
-        $('#recipientEmail').waitForDisplayed({timeout: 20000});
-        $('#recipientEmail').waitForClickable({timeout: 20000});
-        return $('#recipientEmail');
-    }
-    public get AddRecipientBtn() {
-        $('#addRecipientBtn').waitForDisplayed({timeout: 20000});
-        $('#addRecipientBtn').waitForClickable({timeout: 20000});
-        return $('#addRecipientBtn');
-    }
-    public get RuleSaveBtn() {
-        $('#ruleEditSaveBtn').waitForDisplayed({timeout: 20000});
-        $('#ruleEditSaveBtn').waitForClickable({timeout: 20000});
-        return $('#ruleEditSaveBtn');
-    }
-    public get RuleCancelBtn() {
-        $('#ruleEditCancelBtn').waitForDisplayed({timeout: 20000});
-        $('#ruleEditCancelBtn').waitForClickable({timeout: 20000});
-        return $('#ruleEditCancelBtn');
-    }
+  public get RuleCreateBtn() {
+    const ele = $('#ruleCreateBtn');
+    ele.waitForDisplayed({ timeout: 20000 });
+    ele.waitForClickable({ timeout: 20000 });
+    return ele;
+  }
 
-    createNewEform(eFormLabel, selectableReplace, searchableReplace) {
-        this.newEformBtn.click();
-        $('#eFormXml').waitForDisplayed({timeout: 20000});
-        // Create replaced xml and insert it in textarea
-        let xml = XMLForeFormExtended.XML.replace('TEST_LABEL', eFormLabel);
-        xml = xml.replace('REPLACE_SEARCHABLE_ID', searchableReplace);
-        xml = xml.replace('REPLACE_SEARCHABLE_ID', searchableReplace);
-        xml = xml.replace('REPLACE_SINGLE_SELECT_SEARCH_ID', selectableReplace);
-        xml = xml.replace('REPLACE_SINGLE_SELECT_SEARCH_ID', selectableReplace);
-        browser.execute(function (xmlText) {
-            (<HTMLInputElement>document.getElementById('eFormXml')).value = xmlText;
-        }, xml);
-        this.xmlTextArea.addValue(' ');
-        this.createEformTagSelector.click();
-        $('#createEformBtn').waitForDisplayed({timeout: 10000});
-        // browser.pause(5000);
-        this.createEformBtn.click();
-        // browser.pause(14000);
-        $('#delete-eform-btn').waitForDisplayed({timeout: 20000});
-    }
-    public selectOption(option) {
-        $(`//*[text()="${option}"]`).click();
-    }
-    public getFirstRowObject(rowNum): RulesRowObject {
-        browser.pause(500);
-        return new RulesRowObject(rowNum);
-    }
-    public goToMonitoringRulesPage() {
-        this.MonitoringDropdown.click();
-        $('#monitoring-pn-calendar').waitForDisplayed({timeout: 20000});
-        this.MonitoringRulesBtn.click();
-        $('#ruleCreateBtn').waitForDisplayed({timeout: 20000});
-    }
-    public createNewMonitoringRuleWithCheckbox(template, dataField, emailSubject, emailText, recipient) {
-        this.RuleCreateBtn.click();
-        $('#templateSelector').waitForDisplayed({timeout: 20000});
-        this.templateSelector.click();
-        $('#spinner-animation').waitForDisplayed({timeout: 90000, reverse: true});
-        this.selectOption(template);
-        $('#spinner-animation').waitForDisplayed({timeout: 90000, reverse: true});
-        this.DataFieldSelector.click();
-        $('#spinner-animation').waitForDisplayed({timeout: 90000, reverse: true});
-        this.selectOption(dataField);
-        $('#spinner-animation').waitForDisplayed({timeout: 90000, reverse: true});
-        this.CheckboxOption.click();
-        this.EmailSubject.addValue(emailSubject);
-        this.EmailTextArea.addValue(emailText);
-        this.AttachReportBox.click();
-        $('#spinner-animation').waitForDisplayed({timeout: 90000, reverse: true});
-        this.RecipientEmail.addValue(recipient);
-        this.AddRecipientBtn.click();
-        $('#spinner-animation').waitForDisplayed({timeout: 90000, reverse: true});
-        this.RuleSaveBtn.click();
-        $('#spinner-animation').waitForDisplayed({timeout: 90000, reverse: true});
-    }
+  public get templateSelector() {
+    const ele = $('#templateSelector');
+    return ele;
+  }
+
+  public get DataFieldSelector() {
+    const ele = $('#dataFieldSelector');
+    ele.waitForDisplayed({ timeout: 20000 });
+    ele.waitForClickable({ timeout: 20000 });
+    return ele;
+  }
+
+  public get CheckboxOption() {
+    const ele = $('#selected');
+    return ele;
+  }
+
+  public get GreaterThanValue() {
+    const ele = $('#greaterThanValue');
+    ele.waitForDisplayed({ timeout: 20000 });
+    ele.waitForClickable({ timeout: 20000 });
+    return ele;
+  }
+
+  public get LessThanValue() {
+    const ele = $('#lessThanValue');
+    ele.waitForDisplayed({ timeout: 20000 });
+    ele.waitForClickable({ timeout: 20000 });
+    return ele;
+  }
+
+  public get EqualValue() {
+    const ele = $('#equalValue');
+    ele.waitForDisplayed({ timeout: 20000 });
+    ele.waitForClickable({ timeout: 20000 });
+    return ele;
+  }
+
+  public get EmailSubject() {
+    const ele = $('#emailSubject');
+    ele.waitForDisplayed({ timeout: 20000 });
+    ele.waitForClickable({ timeout: 20000 });
+    return ele;
+  }
+
+  public get EmailTextArea() {
+    const ele = $('#emailText');
+    ele.waitForDisplayed({ timeout: 20000 });
+    ele.waitForClickable({ timeout: 20000 });
+    return ele;
+  }
+
+  public get AttachReportBox() {
+    const ele = $('#newEFormBtn');
+    ele.waitForDisplayed({ timeout: 20000 });
+    ele.waitForClickable({ timeout: 20000 });
+    return ele;
+  }
+
+  public get RecipientEmail() {
+    const ele = $('#newEFormBtn');
+    ele.waitForDisplayed({ timeout: 20000 });
+    ele.waitForClickable({ timeout: 20000 });
+    return ele;
+  }
+
+  public get AddRecipientBtn() {
+    const ele = $('#addRecipientBtn');
+    ele.waitForDisplayed({ timeout: 20000 });
+    ele.waitForClickable({ timeout: 20000 });
+    return ele;
+  }
+
+  public get RuleSaveBtn() {
+    const ele = $('#ruleEditSaveBtn');
+    ele.waitForDisplayed({ timeout: 20000 });
+    ele.waitForClickable({ timeout: 20000 });
+    return ele;
+  }
+
+  public get RuleCancelBtn() {
+    const ele = $('#ruleEditCancelBtn');
+    ele.waitForDisplayed({ timeout: 20000 });
+    ele.waitForClickable({ timeout: 20000 });
+    return ele;
+  }
+
+  createNewEform(eFormLabel, selectableReplace, searchableReplace) {
+    myEformsPage.newEformBtn.click();
+    myEformsPage.xmlTextArea.waitForDisplayed({ timeout: 20000 });
+    // Create replaced xml and insert it in textarea
+    let xml = XMLForeFormExtended.XML.replace('TEST_LABEL', eFormLabel);
+    xml = xml.replace('REPLACE_SEARCHABLE_ID', searchableReplace);
+    xml = xml.replace('REPLACE_SEARCHABLE_ID', searchableReplace);
+    xml = xml.replace('REPLACE_SINGLE_SELECT_SEARCH_ID', selectableReplace);
+    xml = xml.replace('REPLACE_SINGLE_SELECT_SEARCH_ID', selectableReplace);
+    browser.execute(function (xmlText) {
+      (<HTMLInputElement>document.getElementById('eFormXml')).value = xmlText;
+    }, xml);
+    myEformsPage.xmlTextArea.addValue(' ');
+    myEformsPage.createEformTagSelector.click();
+    myEformsPage.createEformBtn.waitForDisplayed({ timeout: 10000 });
+    // browser.pause(5000);
+    myEformsPage.createEformBtn.click();
+    // browser.pause(14000);
+    myEformsPage.newEformBtn.waitForDisplayed({ timeout: 20000 });
+  }
+
+  public selectOption(option) {
+    $(`//*[text()="${option}"]`).click();
+  }
+
+  public getFirstRowObject(rowNum): RulesRowObject {
+    browser.pause(500);
+    return new RulesRowObject(rowNum);
+  }
+
+  public goToMonitoringRulesPage() {
+    this.MonitoringDropdown.click();
+    this.monitoringPnCalendar.click();
+  }
+
+  public createNewMonitoringRuleWithCheckbox(
+    template,
+    dataField,
+    emailSubject,
+    emailText,
+    recipient
+  ) {
+    const spinnerAnimation = $('#spinner-animation');
+    this.RuleCreateBtn.click();
+    $('#templateSelector').waitForDisplayed({ timeout: 20000 });
+    this.templateSelector.click();
+    spinnerAnimation.waitForDisplayed({ timeout: 90000, reverse: true });
+    this.selectOption(template);
+    spinnerAnimation.waitForDisplayed({ timeout: 90000, reverse: true });
+    this.DataFieldSelector.click();
+    spinnerAnimation.waitForDisplayed({ timeout: 90000, reverse: true });
+    this.selectOption(dataField);
+    spinnerAnimation.waitForDisplayed({ timeout: 90000, reverse: true });
+    this.CheckboxOption.click();
+    this.EmailSubject.addValue(emailSubject);
+    this.EmailTextArea.addValue(emailText);
+    this.AttachReportBox.click();
+    spinnerAnimation.waitForDisplayed({ timeout: 90000, reverse: true });
+    this.RecipientEmail.addValue(recipient);
+    this.AddRecipientBtn.click();
+    spinnerAnimation.waitForDisplayed({ timeout: 90000, reverse: true });
+    this.RuleSaveBtn.click();
+    spinnerAnimation.waitForDisplayed({ timeout: 90000, reverse: true });
+  }
 }
 
 const rulesPage = new MonitoringNotificationRulesPage();
 export default rulesPage;
 
 export class RulesRowObject {
-    constructor(rowNum) {
-        if ($$('#ruleId')[rowNum - 1]) {
-            this.id = $$('#ruleId')[rowNum - 1];
-            try {
-                this.eFormName = $$('#ruleeFormName')[rowNum - 1].getText();
-            } catch (e) {}
-            try {
-                this.trigger = $$('#ruleTrigger')[rowNum - 1].getText();
-            } catch (e) {}
-            try {
-                this.event = $$('#ruleEvent')[rowNum - 1].getText();
-            } catch (e) {}
-            try {
-                this.editBtn = $$('#updateRuleBtn')[rowNum - 1];
-            } catch (e) {}
-            try {
-                this.deleteBtn = $$('#deleteRuleBtn')[rowNum - 1];
-            } catch (e) {}
-        }
+  constructor(rowNum) {
+    if ($$('#ruleId')[rowNum - 1]) {
+      this.id = $$('#ruleId')[rowNum - 1];
+      try {
+        this.eFormName = $$('#ruleeFormName')[rowNum - 1].getText();
+      } catch (e) {}
+      try {
+        this.trigger = $$('#ruleTrigger')[rowNum - 1].getText();
+      } catch (e) {}
+      try {
+        this.event = $$('#ruleEvent')[rowNum - 1].getText();
+      } catch (e) {}
+      try {
+        this.editBtn = $$('#updateRuleBtn')[rowNum - 1];
+      } catch (e) {}
+      try {
+        this.deleteBtn = $$('#deleteRuleBtn')[rowNum - 1];
+      } catch (e) {}
     }
-    id;
-    eFormName;
-    trigger;
-    event;
-    editBtn;
-    deleteBtn;
+  }
+  id;
+  eFormName;
+  trigger;
+  event;
+  editBtn;
+  deleteBtn;
 }
--- eform-client/e2e/Tests/monitoring-general/monitoring-MonitoringRule-Add.spec.ts
+++ eform-client/e2e/Tests/monitoring-general/monitoring-MonitoringRule-Add.spec.ts
@@ -3,43 +3,50 @@ import loginPage from '../../Page objects/Login.page';
 import myEformsPage from '../../Page objects/MyEforms.page';
 import searchableLists from '../../Page objects/SearchableLists.page';
 import selectableLists from '../../Page objects/SelectableLists.page';
-import {Guid} from 'guid-typescript';
+import { generateRandmString } from '../../Helpers/helper-functions';
 const expect = require('chai').expect;
 
 describe('Monitoring - Monitoring Rules - Add', function () {
-    before(function () {
-        loginPage.open('/auth');
-    });
-    it('should create a searchable, selectable list and eForm', function () {
-        loginPage.login();
-        myEformsPage.Navbar.goToEntitySearch();
-        searchableLists.createSearchableList_NoItem('Searchable');
-        const searchableList = searchableLists.getFirstRowObject();
-        const searchableId = searchableList.id.getText();
-        myEformsPage.Navbar.goToEntitySelect();
-        selectableLists.createSelectableList({ name: 'Selectable'});
-        const selectableList = selectableLists.getFirstSelectableListObject();
-        const selectableId = selectableList.id;
-        loginPage.open('/');
-        $('#spinner-animation').waitForDisplayed({timeout: 90000, reverse: true});
-        rulesPage.createNewEform('Number 1', selectableId, searchableId);
-        $('#spinner-animation').waitForDisplayed({timeout: 90000, reverse: true});
-    });
-    it('should go to monitoring rules page', function () {
-        rulesPage.goToMonitoringRulesPage();
-        $('#spinner-animation').waitForDisplayed({timeout: 90000, reverse: true});
-    });
-    it('should create a new rule with checkbox', function () {
-        const template = 'Test e-mail notifikation på alle felter';
-        const dataField = '2. Sæt flueben';
-        const emailSubject = Guid.create().toString();
-        const emailText = Guid.create().toString();
-        const recipient = 'hej@hej.com';
-        rulesPage.createNewMonitoringRuleWithCheckbox(template, dataField, emailSubject, emailText, recipient);
-        const rule = rulesPage.getFirstRowObject(1);
-        expect(rule.id).equal(1);
-        expect(rule.eFormName).equal(template);
-        expect(rule.trigger).equal('2. Sæt flueben = Checked');
-        expect(rule.event).equal('Email');
-    });
+  before(function () {
+    loginPage.open('/auth');
+    loginPage.login();
+  });
+  it('should create a searchable, selectable list and eForm', function () {
+    const spinnerAnimation = $('#spinner-animation');
+    myEformsPage.Navbar.goToEntitySearch();
+    searchableLists.createSearchableList_NoItem('Searchable');
+    const searchableList = searchableLists.getFirstRowObject();
+    const searchableId = searchableList.id.getText();
+    myEformsPage.Navbar.goToEntitySelect();
+    selectableLists.createSelectableList({ name: 'Selectable' });
+    const selectableList = selectableLists.getFirstSelectableListObject();
+    const selectableId = selectableList.id;
+    loginPage.open('/');
+    spinnerAnimation.waitForDisplayed({ timeout: 90000, reverse: true });
+    rulesPage.createNewEform('Number 1', selectableId, searchableId);
+    spinnerAnimation.waitForDisplayed({ timeout: 90000, reverse: true });
+  });
+  it('should go to monitoring rules page', function () {
+    rulesPage.goToMonitoringRulesPage();
+    $('#spinner-animation').waitForDisplayed({ timeout: 90000, reverse: true });
+  });
+  it('should create a new rule with checkbox', function () {
+    const template = 'Test e-mail notifikation på alle felter';
+    const dataField = '2. Sæt flueben';
+    const emailSubject = generateRandmString();
+    const emailText = generateRandmString();
+    const recipient = 'hej@hej.com';
+    rulesPage.createNewMonitoringRuleWithCheckbox(
+      template,
+      dataField,
+      emailSubject,
+      emailText,
+      recipient
+    );
+    const rule = rulesPage.getFirstRowObject(1);
+    expect(rule.id).equal(1);
+    expect(rule.eFormName).equal(template);
+    expect(rule.trigger).equal('2. Sæt flueben = Checked');
+    expect(rule.event).equal('Email');
+  });
 });
